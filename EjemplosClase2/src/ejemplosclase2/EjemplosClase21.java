7fea175596759ffe16c1263fa23b2d0e9d691610
reroes <reroes799@gmail.com>
2019-04-11 10:45:08 -0500

    agregando información de clase02

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase21.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class EjemplosClase21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Este método es el principal de mi proyecto
+        // No olvidar ingresar valores
+        
+        String nombre;
+        String apellido;
+        Scanner entrada = new Scanner(System.in); 
+        
+        System.out.println("Por favor ingresar sus nombres");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Por favor ingresar sus apellidos");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t Su apellido "
+                + "es: " + apellido);
+                
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+

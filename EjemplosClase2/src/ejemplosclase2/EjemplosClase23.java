dbaafbb648d10a371a3d6a57fc4b40980bcbbb6b
reroes <reroes799@gmail.com>
2019-04-11 11:14:35 -0500

    agregando nuevos archivos

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase23.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class EjemplosClase23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Este método es el principal de mi proyecto
+        // No olvidar ingresar valores
+        
+        String nombre;
+        String apellido;
+        int edad;
+        String ciudad;
+        
+        Scanner entrada = new Scanner(System.in); 
+        
+        System.out.println("Por favor ingresar sus nombres");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Por favor ingresar sus apellidos");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Por favor ingresar su edad");
+        edad = entrada.nextInt();
+        entrada.nextLine(); // limpieza del buffer de entrada de datos
+        
+        System.out.println("Por favor ingresar su ciudad");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t Su apellido "
+                + "es: " + apellido+" "+edad+" "+ciudad);
+                
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
